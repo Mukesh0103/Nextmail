5a2bfdfbf20c055109649b11a567e79dd2fb7c0a
Mukesh0103 <mukeshkannan@users.noreply.github.com>
2020-11-09 14:33:54 +0530

    updated coed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 document.getElementById("more-btn").onclick = function () { myFunction() };
 
 function myFunction() {
-    document.getElementById("hidden-menus").classList.toggle("showMenus");
+  document.getElementById("hidden-menus").classList.toggle("showMenus");
 }
 
 function activeClass(e) {
-    var active = document.querySelectorAll(".active");
-    [].forEach.call(active, function(el) {
-      el.classList.remove("active");
-    });
-    e.target.className = "active";
+  var active = document.querySelectorAll(".active");
+  [].forEach.call(active, function (el) {
+    el.classList.remove("active");
+  });
+  e.target.className = "active";
+}
+
+function openSortDropdown() {
+  var dropdownBox = document.getElementById("sortDropdown");
+  if (dropdownBox.style.display === "block") {
+    dropdownBox.style.display = "none";
+  } else {
+    dropdownBox.style.display = "block";
   }
-  
+  document.addEventListener('mouseup', function (e) {
+    if (!dropdownBox.contains(e.target)) {
+      dropdownBox.style.display = 'none';
+    }
+  });
+}
